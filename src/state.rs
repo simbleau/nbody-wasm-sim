c016c6278e30c0154f3a0da815bbe103af3a61a9
Spencer Imbleau <spencer@imbleau.com>
2022-07-02 19:34:05 -0400

    State now supports resize

--- src/state.rs
+++ src/state.rs
@@ -63,7 +63,12 @@ impl State {
     }
 
     pub fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
-        todo!()
+        if new_size.width > 0 && new_size.height > 0 {
+            self.size = new_size;
+            self.config.width = new_size.width;
+            self.config.height = new_size.height;
+            self.surface.configure(&self.device, &self.config);
+        }
     }
 
     pub fn input(&mut self, event: &WindowEvent) -> bool {
