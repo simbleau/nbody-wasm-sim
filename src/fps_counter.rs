4fb0f1aa679a7e2a26d6f69ac1c092a7f6426e07
Spencer Imbleau <spencer@imbleau.com>
2022-07-02 17:36:38 -0400

    Cleanup

--- src/fps_counter.rs
+++ src/fps_counter.rs
@@ -1,9 +1,7 @@
-use winit::event::Event;
-
 pub fn get_fps_counter() -> web_sys::Element {
     let window = web_sys::window().unwrap();
     let document = window.document().unwrap();
-    let log = document.get_element_by_id("f").unwrap();
+    let counter = document.get_element_by_id("f").unwrap();
 
-    log
+    counter
 }
