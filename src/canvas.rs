a1a81bf6a28327b2006d93987b42aa8d0d312a13
Spencer Imbleau <spencer@imbleau.com>
2022-07-02 17:00:33 -0400

    Code cleanup

--- src/canvas.rs
+++ src/canvas.rs
@@ -7,9 +7,3 @@ pub fn get_canvas() -> HtmlCanvasElement {
     let canvas = document.get_element_by_id("c").unwrap();
     canvas.unchecked_into::<HtmlCanvasElement>()
 }
-
-pub fn style_canvas(canvas: &HtmlCanvasElement) {
-    // Set a background color for the canvas to make it easier to tell the
-    // where the canvas is for debugging purposes.
-    canvas.style().set_css_text("background-color: blue;");
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,14 @@
 mod canvas;
 mod log_list;
 
-use gloo_console::log;
 use wasm_bindgen::prelude::*;
 use winit::dpi::PhysicalSize;
 use winit::platform::web::WindowBuilderExtWebSys;
 use winit::{event::Event, event_loop::EventLoop, window::WindowBuilder};
 
-#[wasm_bindgen(start)]
+#[wasm_bindgen]
 pub fn attach() {
-    log!("Test");
     let canvas = canvas::get_canvas();
-    canvas::style_canvas(&canvas);
     let log_list = log_list::get_log_list();
 
     // Create window
@@ -20,8 +17,7 @@ pub fn attach() {
         .with_canvas(Some(canvas))
         .build(&event_loop)
         .unwrap();
-
-    // Resize canvas
+    // Resize window (the canvas)
     window.set_inner_size(PhysicalSize::new(600.0, 400.0));
 
     // Run program
